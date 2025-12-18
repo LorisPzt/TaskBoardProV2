d49f9b610585ff8c57995101ee6f0854247abe3a
LoloCodiing <loris.pouzet19@gmail.com>
2025-12-18 15:37:52 +0100

    Création du squelette du projet

---
+++ src/app/features/tasks/task-highlight/task-highlight.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-highlight',
+  imports: [],
+  templateUrl: './task-highlight.html',
+  styleUrl: './task-highlight.css',
+})
+export class TaskHighlight {
+
+}
